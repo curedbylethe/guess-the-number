ac1e705bf440015d1b9de96e77535298dcb6f812
Parsa <106576103+curedbylethe@users.noreply.github.com>
2023-04-27 19:54:51 +0330

    Improve styling/logic on the starting screen
    - Added background color
    - Added box shadow
    - Styled the input field to include a border
    - Gave the input field a max length of 2 to avoid users entering numbers larger than 99
    - Set the keyboard type property to number-pad

--- components/screens/StartGameScreen.tsx
+++ components/screens/StartGameScreen.tsx
@@ -1,14 +1,65 @@
-import {TextInput, View} from "react-native";
+import {TextInput, View, StyleSheet} from "react-native";
 import PrimaryButton from "../Button/PrimaryButton";
 
 const StartGameScreen = () => {
     return (
-        <View>
-            <TextInput/>
+        <View style={styles.startGameScreen}>
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.input}
+                           maxLength={2}
+                           keyboardType="number-pad"
+                           autoCapitalize='none'
+                           autoCorrect={false} />
+            </View>
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
     )
 };
+
+const styles = StyleSheet.create({
+    startGameScreen: {
+        // Layout
+        marginTop: 80,
+        marginHorizontal: 20,
+        padding: 16,
+
+        // Border
+        borderWidth: 1,
+        borderRadius: 8,
+        backgroundColor: '#72063c',
+
+        // Shadow
+            // iOS
+            shadowColor: 'black',
+            shadowOffset: {width: 0, height: 2},
+            shadowRadius: 6,
+            shadowOpacity: 0.26,
+
+            // Android
+            elevation: 5,
+    },
+    inputContainer: { // The container that will center the input field
+        // Flexbox
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    input: {
+        // Layout
+        width: 50,
+        height: 50,
+        marginVertical: 8,
+
+        // Border
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+
+        // Font
+        fontSize: 32,
+        fontWeight: 'bold',
+        color: '#ddb52f',
+        textAlign: 'center',
+    }
+});
 
 export default StartGameScreen;
