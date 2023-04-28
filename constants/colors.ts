aabc49dfb9fc87cb6dd6a70904daf3ae09ca6528
Parsa <106576103+curedbylethe@users.noreply.github.com>
2023-04-28 05:33:17 +0330

    Add color constants file
    Grouped colors in a typescript file to make the code more maintainable.

---
+++ constants/colors.ts
@@ -0,0 +1,25 @@
+type ColorsType = {
+    primary: {
+        400: string,
+        500: string,
+        600: string,
+    },
+    accent: {
+        yellow: string,
+        orange: string,
+    },
+}
+
+const Colors: ColorsType = {
+    primary: {
+        400: '#EB455F',
+        500: '#72063c',
+        600: '#842735',
+    },
+    accent: {
+        yellow: '#ddb52f',
+        orange: '#FF6D60',
+    },
+}
+
+export default Colors;
