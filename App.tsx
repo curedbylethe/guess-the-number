7adad0cbbe7b80799fd6b5e46c7cd67efd9e200d
Parsa <106576103+curedbylethe@users.noreply.github.com>
2023-04-28 03:41:31 +0330

    Add background image to starting screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import { StyleSheet } from 'react-native';
+import {ImageBackground, StyleSheet} from 'react-native';
 import StartGameScreen from "./components/screens/StartGameScreen";
 import {LinearGradient} from "expo-linear-gradient";
 
 export default function App() {
   return (
       <LinearGradient colors={['#72063c', '#ddb52f']} style={styles.rootScreen}>
-        <StartGameScreen/>
+          <ImageBackground
+              source={require('./assets/images/background.png')}
+              style={styles.rootScreen}
+              imageStyle={styles.backgroundImage}
+              resizeMode="cover"
+          >
+              <StartGameScreen/>
+          </ImageBackground>
       </LinearGradient>
   );
 }
@@ -15,4 +22,9 @@ const styles = StyleSheet.create({
       // Flexbox
         flex: 1,
     },
+    backgroundImage: {
+        // Color
+            opacity: 0.2,
+    }
+
 });
